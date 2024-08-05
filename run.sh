b5f5a1f694f86bd852d8981754203ce93ade94b4
Alireza Gharib <alirezagharib110@gmail.com>
2024-08-05 20:25:15 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 cd ahmia
 
-MAX_TIME=2160 # 25 days
-FINAL_MAX_TIME=2163 # Finally, wait 60 mins more
+MAX_TIME=2160
+FINAL_MAX_TIME=2163
 
 echo ""
 time timeout --signal=SIGKILL $FINAL_MAX_TIME timeout --kill-after=120 --signal=SIGINT $MAX_TIME scrapy crawl ahmia-tor -s DEPTH_LIMIT=1
